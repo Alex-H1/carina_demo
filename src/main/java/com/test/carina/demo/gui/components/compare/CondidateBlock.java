630442934e20c3952c79edc5a9b4d60e3274950d
alex-h <alexanderohernandez21@gmail.com>
2023-03-09 08:20:31 -0800

    fixed version control

---
+++ src/main/java/com/test/carina/demo/gui/components/compare/CondidateBlock.java
@@ -0,0 +1,35 @@
+package com.test.carina.demo.gui.components.compare;
+
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class CondidateBlock extends AbstractUIObject {
+    @FindBy(xpath = ".//input[contains(@id, 'sSearch')]")
+    private ExtendedWebElement inputField;
+
+    @FindBy(xpath = "./div[contains(@class, 'autocomplete-search')]//a[not(@class)]")
+    private List<ExtendedWebElement> autocompleteSearchElements;
+
+    public CondidateBlock(WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+    public void sendKeysToInputField(String text) {
+        inputField.click();
+        inputField.type(text);
+    }
+
+    public void getFirstPhone() {
+        pause(1);
+        Assert.assertTrue( autocompleteSearchElements.size() > 0, "No phones found!");
+        autocompleteSearchElements.get(0).assertElementPresent();
+        autocompleteSearchElements.get(0).click();
+    }
+}
